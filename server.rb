3c6ca829b274e291a059ef968dad5ce2422d722d
Luis <luis@dvlper.com>
2016-08-19 05:32:23 -0400

    it's 5am

--- server.rb
+++ server.rb
@@ -25,7 +25,7 @@ post '/send' do
       :from => "#{name}<#{sender_email}>",
       :to => 'luis@dvlper.com',
       :subject =>"#{name} is Requesting a Consultation",
-      :body => "#{name}, #{sender_email}, #{message}",
+      :body => "#{name} <br> #{sender_email} <br> #{message}",
     )
     redirect 'http://www.deco27.net/'
   rescue
