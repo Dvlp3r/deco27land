24ff89d97cbf9e10493fb8b74d3635089bad5084
Luis <luis@dvlper.com>
2016-08-19 04:47:20 -0400

    form

--- server.rb
+++ server.rb
@@ -12,3 +12,41 @@ get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
+
+
+
+post '/' do 
+  configure_pony
+  name = params[:name]
+  sender_email = params[:email]
+  message = params[:message]
+  logger.error params.inspect
+  begin
+    Pony.mail(
+      :from => "#{name}<#{sender_email}>",
+      :to => 'example@gmail.com',
+      :subject =>"#{name} has contacted you",
+      :body => "#{message}",
+    )
+    redirect '/success'
+  rescue
+    @exception = $!
+    erb :boom
+  end
+end
+
+def configure_pony
+  Pony.options = {
+    :via => :smtp,
+    :via_options => { 
+      :address              => 'smtp.sendgrid.net', 
+      :port                 => '587',  
+      :user_name            => ENV['SENDGRID_USERNAME'], 
+      :password             => ENV['SENDGRID_PASSWORD'], 
+      :authentication       => :plain, 
+      :enable_starttls_auto => true,
+      :domain               => 'heroku.com'
+    }    
+  }
+end
+
